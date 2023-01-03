913b7cf3971cf9bbd5b7f9a5bc92882b3ef2f964
Falendrac <leger_franck@hotmail.fr>
2023-01-03 16:05:18 +0100

    Add the interaction for the projector

---
+++ Assets/Scripts/ParticleSelector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Activate the particle system when the player select the projector
+/// </summary>
+public class ParticleSelector : MonoBehaviour
+{
+    /// <summary>
+    /// Set active or inactive the gameobject of the projector system
+    /// </summary>
+    public void ActivateParticle()
+    {
+        if (gameObject.activeSelf)
+            gameObject.SetActive(false);
+        else
+            gameObject.SetActive(true);
+    }
+}
